12ee5573bb48db4ec5ec508a40eedbce714c171a
bahugunasiddhant144@gmail.com <bahugunasiddhant144@gmail.com>
2024-09-14 13:50:49 +0530

    data types

---
+++ basics/data_types.js
@@ -0,0 +1,20 @@
+"Use strict";//Treat all js code as newer version
+
+//alert(6+6) // We are using node js,not browser
+console.log(3
+    +3// code readibility should be high
+);
+let name="sid"
+let age=18
+let isloggedin = true
+let state=null
+//number =>2 to the power 53
+//big int 
+// strng=> ""
+// boolean=> true or false
+//undefined
+//symbol =>unique
+
+//object
+
+console.log(typeof undefined);
