de6f67496e46b4b48c19e7b80d0f0a627c645d74
bahugunasiddhant144@gmail.com <bahugunasiddhant144@gmail.com>
2024-09-14 18:21:10 +0530

    Create 3.ConversionsOperations.js

---
+++ basics/3.ConversionsOperations.js
@@ -0,0 +1,28 @@
+//let score=null
+
+//console.log( typeof score )
+
+//let scores="33acsc"
+//console.log(typeof(scores));//string
+
+//let valueInNumber=Number(scores);
+//console.log(valueInNumber);// NaN - not a number
+
+//let css=undefined
+//console.log(Number(css));
+
+// "33"=33
+//"33abs"=>NaN - not a number
+//"true"=>1
+//"false"=>0
+
+// let isloggedIn=0
+
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+let numbers=23
+
+let strnom=String(numbers)
+console.log(typeof strnom);
